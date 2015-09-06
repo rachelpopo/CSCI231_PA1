af02501e647996fb8f66eda5c7530f2dbfb1ed28
Rachel Popo <rach.popo@gmail.com>
2015-09-06 14:26:23 -0600

    Update Point.h

--- Point.h
+++ Point.h
@@ -1,30 +1,41 @@
-// A 2-dimensional point class!
+//
+// Created by Rachel Popo on 9/6/15.
+//
+// A 3-dimensional point class!
 // Coordinates are double-precision floating point.
 
 #ifndef __point_h
 #define __point_h
 
+#include <cmath>
+
 class Point {
 
 private:
-  double x;
-  double y;
+    double x;
+    double y;
+    double z;
 
 public:
-  // Constructors
-  Point();                      // default constructor
-  Point(double x, double y);    // two-argument constructor
+    // Constructors
+    Point();                                // default constructor
+    Point(double x, double y, double z);    // three-argument constructor
 
-  // Destructor
-  ~Point();
+    // Destructor
+    ~Point();
 
-  // Mutator methods
-  void setX(double newX);
-  void setY(double newY);
+    // Mutator methods
+    void setX(double newX);
+    void setY(double newY);
+    void setZ(double newZ);
 
-  // Accessor methods
-  double getX();
-  double getY();
+    // Accessor methods
+    double getX();
+    double getY();
+    double getZ();
+
+    // Find distance between 2 points
+    double distanceTo(Point &p2);
 };
 
 #endif // __point_h
